752a9a13cdb298c89475d92ec295ac2e8bedd6c0
adityakaderiya12 <adityakaderiya12@gmail.com>
2022-11-07 19:55:46 +0545

    Update the data using Put method through Axios

--- src/components/read.js
+++ src/components/read.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Table } from 'semantic-ui-react'
+import { Table, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 export default function Read() {
 	const [APIData, setAPIData] = useState([]);
 useEffect(() => {
@@ -9,6 +10,13 @@ useEffect(() => {
 		setAPIData(response.data);
 })    
 }, [])
+const setData = (data) => {
+	let { id, firstName, lastName, checkbox } = data;
+        localStorage.setItem('ID', id);
+        localStorage.setItem('First Name', firstName);
+        localStorage.setItem('Last Name', lastName);
+        localStorage.setItem('Checkbox Value', checkbox)
+}
     return (
         <div>
             <Table singleLine>
@@ -17,6 +25,7 @@ useEffect(() => {
                         <Table.HeaderCell>First Name</Table.HeaderCell>
                         <Table.HeaderCell>Last Name</Table.HeaderCell>
                         <Table.HeaderCell>Checked</Table.HeaderCell>
+												<Table.HeaderCell>Update</Table.HeaderCell>
                         
                     </Table.Row>
                 </Table.Header>
@@ -28,7 +37,13 @@ useEffect(() => {
                                 <Table.Cell>{data.firstName}</Table.Cell>
                                 <Table.Cell>{data.lastName}</Table.Cell>
                                 <Table.Cell>{data.checkbox ? 'Checked' : 'Unchecked'}</Table.Cell>
-                               
+
+																<Link to='/update'>
+																<Table.Cell> 
+                                  <Button onClick={() => setData(data)}>Update</Button>
+                                </Table.Cell>
+																</Link>
+																
                              
                             </Table.Row>
                         )
